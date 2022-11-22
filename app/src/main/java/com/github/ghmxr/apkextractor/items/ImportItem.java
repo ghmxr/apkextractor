44596e8d46db49ea21628681116ad908c71fca4a
MXR <mxremail@qq.com>
2022-11-22 16:42:57 +0800

    重写importItem中的hashCode方法保证在hashSet中的唯一性（适配器多选）

--- app/src/main/java/com/github/ghmxr/apkextractor/items/ImportItem.java
+++ app/src/main/java/com/github/ghmxr/apkextractor/items/ImportItem.java
@@ -314,4 +314,12 @@ public class ImportItem implements DisplayItem<ImportItem>, Comparable<ImportIte
         }
         return super.equals(obj);
     }
+
+    @Override
+    public int hashCode() {
+        if (fileItem != null) {
+            return fileItem.toString().toLowerCase().hashCode();
+        }
+        return super.hashCode();
+    }
 }
